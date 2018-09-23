c23534c472bb8eb6db5df8a9d079c0ab3b750dd0
Evan Steiner <evanjs@rams.colostate.edu>
2018-09-23 14:04:10 -0600

    First commit, start of chess game, currently single player piece will move to clicked position, starting in x, then z direction

---
+++ Assets/Movement.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+    private Renderer rend;
+    private bool clicked = false;
+    private bool willMove = false;
+    private Transform moveT;
+    // Use this for initialization
+    void Start () {
+        rend = GetComponent<Renderer>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit, 100.0f) && hit.transform != null && hit.transform.gameObject == gameObject)
+            {
+                print(hit.transform.gameObject.name);
+                rend.material.SetColor("_Color", Color.blue);
+                clicked = true;
+
+            }
+            else if(hit.transform == null || !hit.transform.name.Contains("Square"))
+            {
+                rend.material.SetColor("_Color", Color.white);  
+                clicked = false;
+            }else if (hit.transform.name.Contains("Square")){
+                willMove = true;
+                moveT = hit.transform;
+            }
+        }
+        if (willMove) Move();
+    }
+
+    private void Move()
+    {
+
+        if (gameObject.transform.position.x != moveT.position.x)
+        {
+            Vector3 moveX = new Vector3(moveT.position.x, .5f, gameObject.transform.position.z);
+            gameObject.transform.position = Vector3.MoveTowards(gameObject.transform.position, moveX, Time.deltaTime);
+        }else if (gameObject.transform.position.z != moveT.position.z)
+        {
+            Vector3 moveZ = new Vector3(gameObject.transform.position.x, .5f, moveT.position.z);
+            gameObject.transform.position = Vector3.MoveTowards(gameObject.transform.position, moveZ, Time.deltaTime);
+        }
+    }
+}
---
+++ Assets/Scripts/Clickable.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clickable : MonoBehaviour {
+    private Renderer rend;
+    // Use this for initialization
+    public bool clicked = false;
+	void Start () {
+        rend = GetComponent<Renderer>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit, 100.0f) && hit.transform != null && hit.transform.gameObject == gameObject)
+            {
+                print(hit.transform.gameObject.name);
+                rend.material.SetColor("_Color", Color.green);
+                clicked = true;
+         
+            }
+            else
+            {
+                if (gameObject.name.Contains("Black_Square")) rend.material.SetColor("_Color", Color.black);
+                else if (gameObject.name.Contains("White_Square")) rend.material.SetColor("_Color", Color.white);
+                clicked = false;
+            }
+        }
+    }
+}
